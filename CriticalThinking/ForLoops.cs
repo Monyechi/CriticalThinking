d44907f9a1a8efb96b74cb9ccc830294ee7a6c72
Monyechi <monyechi@gmail.com>
2020-04-24 12:35:20 -0400

    Run loop 5x

--- CriticalThinking/ForLoops.cs
+++ CriticalThinking/ForLoops.cs
@@ -15,13 +15,9 @@ namespace CriticalThinking
         // constructor (SPAWNER) 
         public ForLoops()
         {
-            string helloWorld = "Hello World";
             int i;
-            int j;                     
-            
-            j = helloWorld.Length;           
-           
-            for (i = 0; i <= j - 1; i++)
+
+            for (i = 0; i < 5; i++)
             {
                 Console.WriteLine(i);
             }
