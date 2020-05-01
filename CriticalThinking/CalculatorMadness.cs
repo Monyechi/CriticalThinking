6759ed5890747b4234ca338cb43de6110777dbb2
Monyechi <monyechi@gmail.com>
2020-05-01 15:28:25 -0400

    Completed # 1-2 in method

---
+++ CriticalThinking/CalculatorMadness.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriticalThinking
+{    
+    class CalculatorMadness
+    {
+        public void AddTwoNumbers()
+        {
+            Console.WriteLine("Please enter the first number:");
+            int numberOne = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the Second number:");
+            int numberTwo = int.Parse(Console.ReadLine());
+
+            int result = numberOne + numberTwo;
+            Console.WriteLine("The result of this addition is: " + result);
+        }
+
+
+    }
+    
+}
